fa412da6cf8d1823d410d9490bb3b97c7bba232a
robertosamuelx <roberto-samuel1@hotmail.com>
2021-09-09 20:08:18 -0300

    qrcode export

--- src/index.js
+++ src/index.js
@@ -15,25 +15,37 @@ app.listen(process.env.PORT, () => {
 
 venom.create(
     'nex',
-    undefined,
+    async (base64Qr, asciiQR) => {
+        console.log(asciiQR)
+        let matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/)
+
+        if(matches.length !== 3)
+            return new Error('Erro ao gerar QRcode')
+
+        const qrcodeFileName = await file.saveQRCode(matches)
+
+        app.get('/qrcode/refresh', async (req, res) => {
+            return res.sendFile(file.getAbsolutePath(qrcodeFileName))
+        })
+    },
     (statusSession, session) => {
         console.log('Status session:\t' + statusSession)
         console.log('Session:\t' + session)
-    }, 
+    },
     utils.options
-).then( async (client) => {
+).then(async (client) => {
 
     app.post('/response', async (req, res) => {
         const { body } = req
-        await client.sendText((body.to+'@c.us'), body.message)
-        return res.status(200).json({response: 'success'})
+        await client.sendText((body.to + '@c.us'), body.message)
+        return res.status(200).json({ response: 'success' })
     })
 
     app.get('/contacts', async (req, res) => {
         const contacts = await client.getAllContacts()
         const filter = req.headers['filter']
-        const mappedContacts = contacts.map(contact => { return { number: contact.id.user, name: contact.formattedName} })
-        if(filter === "")
+        const mappedContacts = contacts.map(contact => { return { number: contact.id.user, name: contact.formattedName } })
+        if (filter === "")
             return res.status(200).json(mappedContacts)
         else
             return res.status(200).json(mappedContacts.filter(contact => contact.name.includes(filter)))
@@ -45,41 +57,43 @@ venom.create(
         const response = contacts.filter(contact => contact.id.user === id)
         return res.status(200).json(response)
     })
-    
-    client.onMessage( async (message) => {
+
+    client.onMessage(async (message) => {
         const user = message.from
         console.log(user)
 
-        if(message.isMedia === true && message.isGroupMsg == false && user !== 'status'){
-            const buffer = await client.decryptFile(message)
-            
-            const fileName = await file.save(buffer, message)
+        if (message.isGroupMsg == false && user !== 'status') {
+            if (message.isMedia === true) {
+                const buffer = await client.decryptFile(message)
 
-            const result = await api
-                .setUser(user)
-                .setType('media')
-                .setTo(process.env.MY_NUMBER)
-                .setBody('')
-                .sendFileMessage(fileName)
+                const fileName = await file.save(buffer, message)
 
-            if(result.response !== '')
-                await client.sendText(user, result.response).then(() => {console.log('sucess')}).catch(err => {console.log(err)})
-        }
+                const result = await api
+                    .setUser(user)
+                    .setType('media')
+                    .setTo(process.env.MY_NUMBER)
+                    .setBody('')
+                    .sendFileMessage(fileName)
 
-        else {
-            const msg = message.body.toLowerCase()
-    
-            const result = await api
-                .setUser(user)
-                .setBody(msg)
-                .setType('text')
-                .setTo(process.env.MY_NUMBER)
-                .sendMessage()
-    
-            if(result.response !== '')
-                await client.sendText(user, result.response).then(() => {console.log('sucess')}).catch(err => {console.log(err)})
+                if (result.response !== '')
+                    await client.sendText(user, result.response).then(() => { console.log('sucess') }).catch(err => { console.log(err) })
+            }
+
+            else {
+                const msg = message.body.toLowerCase()
+
+                const result = await api
+                    .setUser(user)
+                    .setBody(msg)
+                    .setType('text')
+                    .setTo(process.env.MY_NUMBER)
+                    .sendMessage()
+
+                if (result.response !== '')
+                    await client.sendText(user, result.response).then(() => { console.log('sucess') }).catch(err => { console.log(err) })
+            }
         }
     })
-    }).catch( err => {
-        console.log('Error: '+ err)
-    })
+}).catch(err => {
+    console.log('Error: ' + err)
+})
--- src/services/file.js
+++ src/services/file.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 const mime = require('mime-types')
 const { DateTime } = require('luxon')
 
@@ -12,7 +13,26 @@ module.exports = {
         return fileName
     },
 
+    async saveQRCode(matches){
+        let response = {}
+        const fileName = './tmp/qrcode.png'
+        response.type = matches[1]
+        response.data = new Buffer.from(matches[2], 'base64')
+        await fs.writeFile(fileName, response.data, 'binary', err => {
+            console.error(err)
+        })
+        return fileName
+    },
+
     get(fileName=''){
         return fs.createReadStream(fileName)
+    },
+
+    getAbsolutePath(fileName){
+        if(path.isAbsolute(fileName))
+            return fileName
+        else {
+            return path.resolve(fileName)
+        }
     }
 }
