09caaa1a9442b34f00bdd3f2313ed0392c482858
robertosamuelx <roberto-samuel1@hotmail.com>
2021-09-09 22:47:52 -0300

    add createTmpPath

--- src/services/file.js
+++ src/services/file.js
@@ -4,9 +4,16 @@ const mime = require('mime-types')
 const { DateTime } = require('luxon')
 
 module.exports = {
+
+    createTmpPath(){
+        if(!fs.existsSync('./tmp'))
+            fs.mkdirSync('./tmp')
+    },
+
     async save(buffer, message){
+        this.createTmpPath()
         const now = DateTime.now().setZone('America/Sao_Paulo').toMillis()
-        const fileName = `file_${now}.${mime.extension(message.mimetype)}`
+        const fileName = `./tmp/file_${now}.${mime.extension(message.mimetype)}`
         await fs.writeFile(fileName, buffer, err => {
             console.error(err)
         })
@@ -14,6 +21,7 @@ module.exports = {
     },
 
     async saveQRCode(matches){
+        this.createTmpPath()
         let response = {}
         const fileName = './tmp/qrcode.png'
         response.type = matches[1]
